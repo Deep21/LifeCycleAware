d9608d89e81a4bda324db27e5a68b590af3ceafb
Deeptha WICKREMA <deeptha.wickrema@hotmail.fr>
2018-03-29 17:45:37 +0200

    add dependencies file

---
+++ data/src/main/java/com/example/data/TaskRepository.java
@@ -0,0 +1,36 @@
+package com.example.data;
+
+import com.example.domain.repository.TaskRepositoryInterface;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+/**
+ * Created by afp on 27/03/18.
+ */
+
+public class TaskRepository implements TaskRepositoryInterface {
+
+  public TaskRepository() {
+  }
+
+  @Override
+  public Completable insertOrUpdate() {
+    return null;
+  }
+
+  @Override
+  public Completable delete() {
+    return null;
+  }
+
+  @Override
+  public Single findTaskById() {
+    return null;
+  }
+
+  @Override
+  public Observable getTasks() {
+    return null;
+  }
+}
